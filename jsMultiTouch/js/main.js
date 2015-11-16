10118f2950198683790b57ff0e446a8824f3c650
Arjun Srinivasan <arjun.srinivasan.10@gmail.com>
2015-11-15 22:10:43 -0500

    some console.log statements for easier debugging

--- jsMultiTouch/js/main.js
+++ jsMultiTouch/js/main.js
@@ -15,7 +15,7 @@
     var longPressTimeout = 350;
     var viewCenter = {x:0,y:0};
     var viewZoom = 1;
-    var zoomScale = d3.scale.linear().domain([0,1]).range([1,8]);    
+    var zoomScale = d3.scale.linear().domain([0,1]).range([1,4]);    
 
     main.graph = {
         "nodes":[
@@ -118,20 +118,24 @@
             handleNewTouch(event);
 
             // Create a new press object to handle this event stream
-            press = {time: event.timestamp, centers: [event.center], events: [event], touches: event.pointers.length};
+            press = {time: event.timeStamp, centers: [event.center], events: [event], touches: event.pointers.length};
+
+            // are these needed? There will never be a single press event (since the minimum is touchstart and touchend)
             if(event.pointers.length == 1) {
                 handleSinglePressEvent(event);
             } else {
                 handleMultiPressEvent(event);
-            }
+            }   
+            console.log("touch down")         
         } else {
             // Check if this event moved enough, then start a swipe
-            if(!swipe && event.distance > moveThreshold) {
+            if(!swipe && event.distance > moveThreshold) { // TODO: check the 2nd condition since it is invoking a touchend event (2nd case below) at the start of two-finger swipe
                 press.centers.push(event.center);
                 press.events.push(event);
-                swipe = {time: event.timestamp, centers: press.centers, events: press.events, touches: event.pointers.length};
+                swipe = {time: event.timeStamp, centers: press.centers, events: press.events, touches: event.pointers.length};
             }
             if (swipe) {
+                console.log("swipe")
                 swipe.time = event.timestamp;
                 swipe.centers.push(event.center);
                 swipe.events.push(event);
@@ -139,7 +143,7 @@
                 // if a swipe starts out as one finger but ends as two we want it to be a double swipe
 
                 // WHAT HAPPENS WHEN WE GO FROM TWO FINGERS TO ONE? this condition sets swipe.touches to 2 in that case. This is causing the jump at the end of a pan event.
-                swipe.touches = event.pointers.length;//swipe.touches > event.pointers.length ? swipe.touches : event.pointers.length;
+                swipe.touches =  swipe.touches > event.pointers.length ? swipe.touches : event.pointers.length;
                 
 
                 // TODO handle a switch so that we override any events that were triggered from a different swipe
@@ -153,12 +157,13 @@
                     handleHandSwipeEvent(event);
                 }
             } else {
+                console.log("touch up")
                 press.time = event.timestamp;
                 press.centers.push(event.center);
                 press.events.push(event);
                 // Always take the greater of the two
                 // if a press starts out as one finger but ends as two we want it to be a double press
-                press.touches = press.touches > event.pointers.length ? press.touches : event.pointers.length;
+                press.touches = press.touches > event.pointers.length ? press.touches : event.pointers.length;                
                 if(press.touches == 1) {
                     handleSinglePressEvent(event);
                 } else {
@@ -181,7 +186,10 @@
     }
 
     function handleSingleSwipeEvent(event) {
-
+        if(event.isFinal==true){ // checks if it is the end of a swipe event, if yes then resets the swipe.centers list
+           swipe = null;
+           console.log("single swipe end")
+        }        
     }
 
     function handleDoubleSwipeEvent(event) { // ISSUE: both events are always triggered together (might need to re-think about these interactions).
@@ -205,22 +213,27 @@
 
         if(event.isFinal==true){ // checks if it is the end of a swipe event, if yes then resets the swipe.centers list
            swipe = null;
+           console.log("two finger swipe end")
         }
 
 
         var point1 = event.pointers[0];
         var point2 = event.pointers[1];        
-        var zoomDelta = curScale-prevScale;;
+        var zoomDelta = curScale-prevScale;
+        // offsetX = -(curPointX * zoomDelta);
+        // offsetY = -(curPointY * zoomDelta);
+
         if(zoomDelta>0){
-            console.log("pinch out");
+            // console.log("pinch out");
         }else{
-            console.log("pinch in");
+            // console.log("pinch in");
         }
         //console.log(event.pointers.length)
         viewZoom += zoomDelta;
         viewCenter.x += deltaX;
-        viewCenter.y += deltaY;        
+        viewCenter.y += deltaY;      
         // container.attr("transform","translate("+viewCenter.x+","+viewCenter.y+")")
+        // container.attr("transform","translate("+viewCenter.x+","+viewCenter.y+")scale("+zoomScale(viewZoom)+")")
         // container.attr("transform","scale("+zoomScale(viewZoom)+")")
     }
 
@@ -235,7 +248,7 @@
     function getPassThroughLinks(p1,p2) {
         var select = '';
         d3.selectAll('.link').each(function(l){
-            if(lineIntersect(p1.x,p1.y,p2.x,p2.y,l.source.x,l.source.y,l.target.x,l.target.y)) {
+            if(lineIntersect(p1.x,p1.y,p2.x,p2.y,l.source.x,l.source.y,l.target.x,l.target.y)) { 
                 select += '#'+l.source.name+'-'+l.target.name+', ';
             }
         });
